b715480dfa9c5fb62b477057014d967aca5306b0
5358979323 <robstsalat@protonmail.com>
2023-07-24 19:40:52 +0100

    Produkt- und Materialliste mit Entfernen-Option

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -2,107 +2,91 @@
     <div>
         <h1>Gesammelte Produkte</h1>
         <ol>
-            <li v-for="produkt in daten.produkte" :key="produkt._id">{{ produkt.name }}
-                <button @click="removeProdukt(produkt.produktId)">Entfernen</button>
+            <li v-for="produkt in daten.produkte" :key="produkt._id">{{ produkt._id }}
+                <button @click="removeProdukt(produkt._id)">Entfernen</button>
             </li>
         </ol>
-    </div> 
+    </div>
+    <div>
+        <h1>Materialien-Fundus</h1>
+        <ol>
+            <li v-for="material in materialDaten.materialien" :key="material._id">{{ material._id }}
+                <button @click="removeMaterial(material._id)">Entfernen</button>
+            </li>
+        </ol>
+    </div>
 </template> 
+
 <script>
-/*import { onMounted, reactive, ref } from "vue";
 import PouchDB from "pouchdb-browser";
-import { useRoute, useRouter } from "vue-router";
-
-export default {
-
-    setup() {
-        const produkte = reactive([])
-        const db = new PouchDB("Produkte")
-        const rDb = new PouchDB("https://apartofnature.info/produkte", {
-            auth: {
-                username: "rezeptator",
-                password: "IkolpingT2023"
-            }
-        })
-
-
-        db.sync(rDb, {
-            live: true,
-            retry: true
-        }).on('error', function (err) {
-            console.log("Produkte Sync Error")
-        });
-
-        const state = reactive({
-            allPro: []
-        })
-        async function loadProducts() {
-            produkte.push(...state.allPro)
-            
-         /* try {
-             const result = await db.allDocs({ include_docs: true });
-             state.allPro = result.rows.filter(row => !row.id.startsWith('_design/')).map(row => row.doc)
-
-         } catch (error) {
-             console.error('Error retrieving products:', error)
-          }
-         }
-        }
-
-        onMounted(() => {
-            loadProducts();
-            
-            
-        });
-
-        
-      
-        return {
-            state,
-            loadProducts,
-            removeProdukt
-        };
-    }
-}; */
-//return {
- //   produkte: state.produkte,
-  //  removeProdukt,
-//};
-   // },
-//};
-import PouchDB from "pouchdb-browser";
-
-const db = new PouchDB("Produkte");
-async function loadDaten() {
-  try {
-    const result = await db.allDocs({ include_docs: true });
-    const daten = result.rows.map(row => row.doc);
-    return daten;
-  } catch (error) {
-    console.error("Fehler beim Abrufen der Daten:", error);
-    return [];
-  }
-}
 import { reactive } from "vue";
 
-export default {
-  setup() {
-    const daten = reactive({
-      produkte: [],
-    });
+const db = new PouchDB("Produkte");
+const mdb = new PouchDB("Materialien");
 
-    
-    loadDaten().then(data => {
-      daten.produkte = data;
-    });
-    function removeProdukt(produktId) {
+async function loadDaten() {
+    try {
+        const result = await db.allDocs({ include_docs: true });
+        const daten = result.rows.map(row => row.doc);
+        return daten;
+    } catch (error) {
+        console.error("Fehler beim Abrufen der Daten:", error);
+        return [];
+    }
+}
+async function loadMaterial() {
+    try {
+        const mresult = await mdb.allDocs({ include_docs: true });
+        const materialDaten = mresult.rows.map(row => row.doc);
+        return materialDaten;
+    } catch (error) {
+        console.error("Fehler beim Abrufen des Materials:", error);
+        return [];
+    }
+};
+
+
+export default {
+    setup() {
+        const daten = reactive({
+            produkte: [],
+        });
+
+
+        loadDaten().then(data => {
+            daten.produkte = data;
+        });
+
+        function removeProdukt(produktId) {
+            const index = daten.produkte.findIndex((produkt) => produkt._id === produktId);
+            if (index !== -1) {
+                daten.produkte.splice(index, 1);
+            }
             console.log("Produkt entfernen:", produktId);
         }
+        const materialDaten = reactive({
+            materialien: [],
+        });
 
-    return {
-      daten,
-      removeProdukt,
-    };
-  },
+
+        loadMaterial().then(data => {
+            materialDaten.materialien = data;
+        });
+
+        function removeMaterial(materialId) {
+            const index = materialDaten.materialien.findIndex((material) => material._id === materialId);
+            if (index !== -1) {
+                materialDaten.materialien.splice(index, 1);
+            }
+            console.log("Material entfernen:", materialId);
+        }
+        return {
+            materialDaten,
+            removeMaterial,
+            daten,
+            removeProdukt,
+        };
+
+    },
 };
 </script>
